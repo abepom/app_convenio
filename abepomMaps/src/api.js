417e435ef002163af65c5e3339cd532aaa3ba576
Thiago-ABEPOM <thiago.denir.ramos@hotmail.com>
2020-03-04 10:26:16 -0300

    Pronto

--- abepomMaps/src/api.js
+++
@@ -1,6 +0,0 @@
-import Axios from 'axios';
-
-const api = Axios.create({
-  baseURL: 'http://187.94.98.194:3333',
-});
-export default api;
--- abepomMaps/src/components/StatusBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-
-const NotificationsBar = () => {
-  return <StatusBar backgroundColor="#1f4ba4" barStyle="light-content" />;
-};
-
-export default NotificationsBar;
