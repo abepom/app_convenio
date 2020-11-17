d7ccced8ece03851a07a5a6db1f9a3a44c79db96
Thiago denir Ramos <thiago.ramos@abepom.com.br>
2020-11-17 13:17:26 -0300

    ok

--- abepom_farmacia/src/Screens/Load.js
+++ abepom_farmacia/src/Screens/Load.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, Platform } from "react-native";
 import logo from "./../../assets/img/logo_abepom_branca.png";
 import styles, { primary } from "./../utils/Style";
 import api from "../api";
@@ -11,93 +11,112 @@ import useConvenio from "../Data/Convenio";
 import Carregando from "../components/Carregando";
 import * as Permissions from "expo-permissions";
 const Load = (props) => {
-  const { navigation } = props;
+	const { navigation } = props;
 
-  const [usuario] = useUsuario();
-  const [store] = useStore();
-  const [, setConv] = useConvenio();
-  useEffect(() => {
-    if (!!store.carregouDados) {
-      conectar();
-    }
-  }, [store.carregouDados]);
+	const [usuario] = useUsuario();
+	const [store] = useStore();
+	const [, setConv] = useConvenio();
+	useEffect(() => {
+		if (!!store.carregouDados) {
+			conectar();
+		}
+	}, [store.carregouDados]);
 
-  const conectar = async () => {
-    try {
-      await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      
-      const token = 'teste'//await (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-      if (usuario === undefined) {
-        setTimeout(() => {
-          return navigation.navigate("Login");
-        }, 2000);
-      }
+	const conectar = async () => {
+		try {
+			let token;
+			if (Constants.isDevice) {
+				const { status: existingStatus } = await Permissions.getAsync(
+					Permissions.NOTIFICATIONS
+				);
+				let finalStatus = existingStatus;
+				if (existingStatus !== "granted") {
+					const { status } = await Permissions.askAsync(
+						Permissions.NOTIFICATIONS
+					);
+					finalStatus = status;
+				}
+				if (finalStatus !== "granted") {
+					console.log("Ocorreu falha para capturar o push token notification.");
+					return;
+				}
+				token = await Notifications.getExpoPushTokenAsync();
+			} else {
+				if (Platform.OS == "ios") {
+					token = { data: "simuladorIOS" };
+				} else {
+					token = await (await Notifications.getExpoPushTokenAsync()).data;
+				}
+			}
 
-      const { doc, user, pass } = usuario;
+			if (usuario === undefined) {
+				setTimeout(() => {
+					return navigation.navigate("Login");
+				}, 2000);
+			}
 
-      if (!!doc && !!pass) {
-        const { data } = await api.post("/Login", {
-          doc: doc,
-          senha: pass,
-          user,
-          token,
-        });
+			const { doc, user, pass } = usuario;
 
-        console.log("aqui 1");
-        let convenio;
-        if (!data.erro) {
-          convenio = {
-            id_gds: data.id_gds,
-            nome_parceiro: data.nome_parceiro,
-            caminho_logomarca: `${data.caminho_logomarca}?id=${Math.random()}`,
-            efetuarVenda: data.efetuarVenda,
-            doc: data.doc,
-            usuario: data.usuario,
-            nivel: data.nivel,
-            token,
-            cd_convenio: data["cd_convênio"],
-            primeiro_acesso: data.primeiro_acesso,
-          };
+			if (!!doc && !!pass) {
+				const { data } = await api.post("/Login", {
+					doc: doc,
+					senha: pass,
+					user,
+					token,
+				});
 
-          setTimeout(() => {
-            navigation.navigate("App", convenio);
-            setConv(convenio);
-          }, 2000);
-          console.log("data");
-        } else {
-          setTimeout(() => {
-            navigation.navigate(
-              "Login",
-              {
-                ...data,
-                doc,
-                mensagem: "Senha não confere, digite novamene a sua senha",
-              },
-              3000
-            );
-          });
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+				let convenio;
+				if (!data.erro) {
+					convenio = {
+						id_gds: data.id_gds,
+						nome_parceiro: data.nome_parceiro,
+						caminho_logomarca: `${data.caminho_logomarca}?id=${Math.random()}`,
+						efetuarVenda: data.efetuarVenda,
+						doc: data.doc,
+						usuario: data.usuario,
+						nivel: data.nivel,
+						token,
+						cd_convenio: data["cd_convênio"],
+						primeiro_acesso: data.primeiro_acesso,
+					};
 
-  return (
-    <View
-      style={{
-        width: "100%",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: primary,
-      }}
-    >
-      <StatusBar />
-      <Carregando tamanho={150} cor={"white"} />
-    </View>
-  );
+					setTimeout(() => {
+						navigation.navigate("App", convenio);
+						setConv(convenio);
+					}, 2000);
+					console.log("data");
+				} else {
+					setTimeout(() => {
+						navigation.navigate(
+							"Login",
+							{
+								...data,
+								doc,
+								mensagem: "Senha não confere, digite novamene a sua senha",
+							},
+							3000
+						);
+					});
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<View
+			style={{
+				width: "100%",
+				height: "100%",
+				alignItems: "center",
+				justifyContent: "center",
+				backgroundColor: primary,
+			}}>
+			<StatusBar />
+			<Carregando tamanho={150} cor={"white"} />
+		</View>
+	);
 };
 
 export default Load;
--- abepom_farmacia/src/Screens/Login.js
+++ abepom_farmacia/src/Screens/Login.js
@@ -5,11 +5,11 @@ import StatusBar from "../components/StatusBar";
 import logo from "../../assets/img/logo_abepom_branca.png";
 
 import styles, {
-  danger,
-  danverBackground,
-  primary,
-  sucess,
-  background,
+	danger,
+	danverBackground,
+	primary,
+	sucess,
+	background,
 } from "../utils/Style";
 import api from "./../api";
 import { ScrollView } from "react-native-gesture-handler";
@@ -23,198 +23,221 @@ import * as Notifications from "expo-notifications";
 const initialLayout = { width: Dimensions.get("window").width };
 
 const Login = (props) => {
-  const [index, setIndex] = useState(0);
-  const [carregando, setCarregando] = useState(false);
+	const [index, setIndex] = useState(0);
+	const [carregando, setCarregando] = useState(false);
 
-  const [reset, setReset] = useState(
-    props.navigation.state.params
-      ? props.navigation.state.params.resetSenha
-      : false
-  );
-  const [state, setState] = useState({
-    erro: false,
-    mensagem: "",
-  });
+	const [reset, setReset] = useState(
+		props.navigation.state.params
+			? props.navigation.state.params.resetSenha
+			: false
+	);
+	const [state, setState] = useState({
+		erro: false,
+		mensagem: "",
+	});
 
-  const [, setUsuario] = useUsuario();
-  const [, setConv] = useConvenio();
-  useEffect(() => {
-    if (state.erro) {
-      setTimeout(() => {
-        setState({ ...state, erro: false });
-      }, 6000);
-    }
-  }, [state.erro]);
+	const [, setUsuario] = useUsuario();
+	const [, setConv] = useConvenio();
+	useEffect(() => {
+		if (state.erro) {
+			setTimeout(() => {
+				setState({ ...state, erro: false });
+			}, 6000);
+		}
+	}, [state.erro]);
 
-  useEffect(() => {
-    if (props.navigation.state.params) {
-      if (props.navigation.state.params.index) {
-        setIndex(props.navigation.state.params.index);
-      }
+	useEffect(() => {
+		if (props.navigation.state.params) {
+			if (props.navigation.state.params.index) {
+				setIndex(props.navigation.state.params.index);
+			}
 
-      setState(props.navigation.state.params);
-    }
-  }, []);
+			setState(props.navigation.state.params);
+		}
+	}, []);
 
-  const getToken = async () => {
-      const token = 'teste'//await (await Notifications.getExpoPushTokenAsync()).data;
-    return token.data;
-  };
+	const getToken = async () => {
+		let token;
+		if (Constants.isDevice) {
+			const { status: existingStatus } = await Permissions.getAsync(
+				Permissions.NOTIFICATIONS
+			);
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const { status } = await Permissions.askAsync(
+					Permissions.NOTIFICATIONS
+				);
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				console.log("Ocorreu falha para capturar o push token notification.");
+				return;
+			}
+			token = await Notifications.getExpoPushTokenAsync();
+		} else {
+			if (Platform.OS == "ios") {
+				token = { data: "simuladorIOS" };
+			} else {
+				token = await (await Notifications.getExpoPushTokenAsync()).data;
+			}
+		}
 
-  const [routes] = useState([
-    { key: "1", title: "Administrador" },
-    { key: "2", title: "Ponto de venda" },
-  ]);
+		return token.data;
+	};
 
-  const renderScene = SceneMap({
-    2: () => (
-      <LoginPDV
-        {...props}
-        carregando={carregando}
-        setState={setState}
-        state={state}
-        reset={reset}
-        func={conectar}
-        setReset={setReset}
-      />
-    ),
-    1: () => (
-      <LoginAdm
-        {...props}
-        carregando={carregando}
-        setState={setState}
-        state={state}
-        reset={reset}
-        func={conectar}
-        setReset={setReset}
-      />
-    ),
-  });
+	const [routes] = useState([
+		{ key: "1", title: "Administrador" },
+		{ key: "2", title: "Ponto de venda" },
+	]);
 
-  const conectar = async (imput) => {
-    setCarregando(true);
-    console.log("teste");
-    const { doc, user, pass } = imput;
-    let token = await getToken();
+	const renderScene = SceneMap({
+		2: () => (
+			<LoginPDV
+				{...props}
+				carregando={carregando}
+				setState={setState}
+				state={state}
+				reset={reset}
+				func={conectar}
+				setReset={setReset}
+			/>
+		),
+		1: () => (
+			<LoginAdm
+				{...props}
+				carregando={carregando}
+				setState={setState}
+				state={state}
+				reset={reset}
+				func={conectar}
+				setReset={setReset}
+			/>
+		),
+	});
 
-    if (doc.length > 1 && pass) {
-      try {
-        const { data } = await api.post("/Login", {
-          doc: doc,
-          senha: pass,
-          user,
-          token,
-        });
+	const conectar = async (imput) => {
+		setCarregando(true);
+		console.log("teste");
+		const { doc, user, pass } = imput;
+		let token = await getToken();
 
-        let convenio;
-        if (!data.erro) {
-          setUsuario(imput);
-          convenio = {
-            id_gds: data.id_gds,
-            nome_parceiro: data.nome_parceiro,
-            caminho_logomarca: `${data.caminho_logomarca}?id=${Math.random()}`,
-            efetuarVenda: data.efetuarVenda,
-            doc: data.doc,
-            usuario: data.usuario,
-            nivel: data.nivel,
-            token,
-            cd_convenio: data["cd_convênio"],
-            primeiro_acesso: data.primeiro_acesso,
-          };
+		if (doc.length > 1 && pass) {
+			try {
+				const { data } = await api.post("/Login", {
+					doc: doc,
+					senha: pass,
+					user,
+					token,
+				});
 
-          setConv(convenio);
-          props.navigation.navigate("App");
-        } else {
-          setCarregando(false);
+				let convenio;
+				if (!data.erro) {
+					setUsuario(imput);
+					convenio = {
+						id_gds: data.id_gds,
+						nome_parceiro: data.nome_parceiro,
+						caminho_logomarca: `${data.caminho_logomarca}?id=${Math.random()}`,
+						efetuarVenda: data.efetuarVenda,
+						doc: data.doc,
+						usuario: data.usuario,
+						nivel: data.nivel,
+						token,
+						cd_convenio: data["cd_convênio"],
+						primeiro_acesso: data.primeiro_acesso,
+					};
 
-          setState({ erro: true, mensagem: "Usuário ou Senha incorretos" });
-        }
-      } catch (error) {
-        alert(`${error}  	`);
-      }
-    } else {
-      setState({ erro: true, mensagem: "Usuário ou Senha incorretos" });
-    }
-  };
+					setConv(convenio);
+					props.navigation.navigate("App");
+				} else {
+					setCarregando(false);
 
-  return (
-    <>
-      <StatusBar />
-      {/* <View style={estilos.conteiner}>
+					setState({ erro: true, mensagem: "Usuário ou Senha incorretos" });
+				}
+			} catch (error) {
+				alert(`${error}  	`);
+			}
+		} else {
+			setState({ erro: true, mensagem: "Usuário ou Senha incorretos" });
+		}
+	};
+
+	return (
+		<>
+			<StatusBar />
+			{/* <View style={estilos.conteiner}>
         <Text>teste</Text> */}
-      <ScrollView
-        style={{ width: "100%", height: "100%", backgroundColor: primary }}
-      >
-        <Image
-          style={[styles.logo, { marginTop: "10%", alignSelf: "center" }]}
-          source={logo}
-        />
-        <View style={{ alignItems: "center" }}>
-          <Text style={[styles.white, styles.textoGG]}>ABEPOM</Text>
-          <Text style={[styles.white, styles.textoM]}>FARMÁCIA</Text>
-        </View>
+			<ScrollView
+				style={{ width: "100%", height: "100%", backgroundColor: primary }}>
+				<Image
+					style={[styles.logo, { marginTop: "10%", alignSelf: "center" }]}
+					source={logo}
+				/>
+				<View style={{ alignItems: "center" }}>
+					<Text style={[styles.white, styles.textoGG]}>ABEPOM</Text>
+					<Text style={[styles.white, styles.textoM]}>FARMÁCIA</Text>
+				</View>
 
-        <TabView
-          navigationState={{ index, routes }}
-          renderScene={renderScene}
-          onIndexChange={setIndex}
-          style={{
-            marginTop: 20,
-            flex: 1,
-          }}
-          initialLayout={initialLayout}
-          renderTabBar={(props) => (
-            <TabBar
-              {...props}
-              indicatorStyle={{ backgroundColor: "white" }}
-              style={{ backgroundColor: primary, elevation: 1 }}
-              labelStyle={styles.textoM}
-            />
-          )}
-        />
-      </ScrollView>
-      {/* </View> */}
-    </>
-  );
+				<TabView
+					navigationState={{ index, routes }}
+					renderScene={renderScene}
+					onIndexChange={setIndex}
+					style={{
+						marginTop: 20,
+						flex: 1,
+					}}
+					initialLayout={initialLayout}
+					renderTabBar={(props) => (
+						<TabBar
+							{...props}
+							indicatorStyle={{ backgroundColor: "white" }}
+							style={{ backgroundColor: primary, elevation: 1 }}
+							labelStyle={styles.textoM}
+						/>
+					)}
+				/>
+			</ScrollView>
+			{/* </View> */}
+		</>
+	);
 };
 const estilos = StyleSheet.create({
-  buttonView: {
-    position: "relative",
-    flex: 1,
-    justifyContent: "space-around",
+	buttonView: {
+		position: "relative",
+		flex: 1,
+		justifyContent: "space-around",
 
-    marginHorizontal: "10%",
-    marginVertical: 10,
-    flexDirection: "row",
-  },
-  retornoBackend: {
-    width: "80%",
-    borderRadius: 5,
-    alignItems: "center",
-    margin: 5,
-    padding: 10,
-    alignSelf: "center",
-  },
-  mensagemErro: {
-    backgroundColor: danverBackground,
-    borderColor: danger,
-  },
-  mensagemSucesso: {
-    backgroundColor: sucess,
-    borderColor: sucess,
-  },
+		marginHorizontal: "10%",
+		marginVertical: 10,
+		flexDirection: "row",
+	},
+	retornoBackend: {
+		width: "80%",
+		borderRadius: 5,
+		alignItems: "center",
+		margin: 5,
+		padding: 10,
+		alignSelf: "center",
+	},
+	mensagemErro: {
+		backgroundColor: danverBackground,
+		borderColor: danger,
+	},
+	mensagemSucesso: {
+		backgroundColor: sucess,
+		borderColor: sucess,
+	},
 
-  cabecalho: {
-    color: "white",
-    width: "80%",
-    marginTop: 20,
-    textAlign: "justify",
-  },
-  conteiner: {
-    flex: 1,
-    backgroundColor: primary,
-    alignItems: "center",
-  },
+	cabecalho: {
+		color: "white",
+		width: "80%",
+		marginTop: 20,
+		textAlign: "justify",
+	},
+	conteiner: {
+		flex: 1,
+		backgroundColor: primary,
+		alignItems: "center",
+	},
 });
 
 export default Login;
