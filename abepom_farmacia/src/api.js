13afeac9ec343266b43c441a9602892cda475466
Thiago denir Ramos <thiago.ramos@abepom.com.br>
2020-12-03 13:46:54 -0300

    teste de comit

--- abepom_farmacia/src/api.js
+++ abepom_farmacia/src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 // TESTE;
 const api = axios.create({
-  baseURL: "http://187.94.98.194:3916",
+	baseURL: "http://187.94.98.194:3917/convenios",
 });
 
 // OFICIAL
 // const api = axios.create({
-//   baseURL: 'http://mobile.abepom.org.br:3972',
+//   baseURL: 'http://api.abepom.org.br:3972/convenios',
 // });
 export default api;
