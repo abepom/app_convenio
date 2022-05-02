1ed7260159a06b12c2ccd8c08dc4df47b1383a00
fernando <nti@abepom.org.br>
2022-05-02 16:14:45 -0300

    Ajuste de mensagem

--- src/components/Modal/ValidarLancamento.modal.js
+++ src/components/Modal/ValidarLancamento.modal.js
@@ -127,8 +127,8 @@ const ModalValidarLancamento = (props) => {
 							}}>
 							<Text>
 								{!!CelularBanco
-									? "Confirme o celular do associado/dependente para envio do código de autorização de lançamento."
-									: "Solicite o celular do associado/dependente para envio do código de autorização de lançamento."}
+									? "Confirme "
+									: "Solicite "} o celular do associado/dependente para envio do código de autorização de lançamento.
 							</Text>
 							<View style={{ flexDirection: "row" }}>
 								{!!CelularBanco ? (
