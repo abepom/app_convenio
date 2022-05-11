a761eec39cd8098d38e9f6f0b4de7c34481babc8
Thiago Denir Ramos <thiago.denir.ramos@hotmail.com>
2022-05-11 08:32:21 -0300

    ajuste lançamento

--- src/api.js
+++ src/api.js
@@ -5,12 +5,12 @@ import axios from "axios";
 // });
 
 // TESTE;
-const api = axios.create({
-	baseURL: "http://apiteste.abepom.org.br:3917/convenios",
-});
+// const api = axios.create({
+// 	baseURL: "http://apiteste.abepom.org.br:3917/convenios",
+// });
 
 // OFICIAL
-// const api = axios.create({
-// 	baseURL: "http://api.abepom.org.br:3972/convenios",
-// });
+const api = axios.create({
+	baseURL: "http://api.abepom.org.br:3972/convenios",
+});
 export default api;
